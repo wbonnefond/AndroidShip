0296c4378045bfc8d38650e9f5843254059e13e1
Ward Bonnefond <wbonnefond@gmail.com>
2012-04-16 23:33:17 -0400

    3rd commit

---
+++ AndroidShip/src/cs2114/AndroidShip/AndroidShipView.java
@@ -0,0 +1,16 @@
+package cs2114.AndroidShip;
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author Ward-Laptop
+ *  @version Apr 16, 2012
+ */
+
+public class AndroidShipView
+{
+
+}
---
+++ AndroidShip/src/cs2114/AndroidShip/Cell.java
@@ -0,0 +1,6 @@
+package cs2114.AndroidShip;
+
+public class Cell
+{
+
+}
