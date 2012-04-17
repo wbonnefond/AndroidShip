d74da8075bfd8dd515c07944777f5e62c97df0c0
ritchie4 <ritchie4@vt.edu>
2012-04-17 15:14:20 -0400

    Method declarations for the model created.

--- AndroidShip/src/cs2114/AndroidShip/AndroidShipModel.java
+++ AndroidShip/src/cs2114/AndroidShip/AndroidShipModel.java
@@ -1,15 +1,40 @@
 package cs2114.AndroidShip;
 
-// -------------------------------------------------------------------------
 /**
- *  Write a one-sentence summary of your class here.
- *  Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ *  This defines the model for the AndroidShip game.
  *
- *  @author Tyler
- *  @version Apr 17, 2012
+ *  @author Ward Bonnefond (wardwdb)
+ *  @author Tyler Ritchie (ritchie4)
+ *  @author Nick Crowder (crowdern)
+ *  @version Apr 16, 2012
  */
+
 public class AndroidShipModel
 {
+    /**
+     * The constructor for this model.
+     */
+    public AndroidShipModel()
+    {
+    }
 
+    /**
+     * Fires at a specific coordinate on a specified board.
+     * @param x The x coordinate of the fire target
+     * @param y The y coordinate of the fire target
+     * @param board The board to fire on, usually the opponent's board
+     */
+    public void fireAtCoordinate(int x, int y, AndroidShipBoard board)
+    {
+        // todo
+    }
+
+    /**
+     * Switches the labels, boards, and status labels when the phone is
+     * handed to the other player after a turn.
+     */
+    public void switchPlayers()
+    {
+        // todo
+    }
 }
