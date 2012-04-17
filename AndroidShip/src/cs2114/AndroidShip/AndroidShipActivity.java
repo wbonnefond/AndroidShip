78963362856713d4ea81cc3177a895bb44652518
ritchie4 <ritchie4@vt.edu>
2012-04-17 00:21:00 -0400

    Pushing my starting files..

--- AndroidShip/src/cs2114/AndroidShip/AndroidShipActivity.java
+++ AndroidShip/src/cs2114/AndroidShip/AndroidShipActivity.java
@@ -1,13 +1,78 @@
-package cs2114.AndroidShip;
+package cs2114.mazesolver;
 
+import android.widget.TextView;
+import android.widget.CheckBox;
+import android.view.View;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class AndroidShipActivity extends Activity {
-    /** Called when the activity is first created. */
+/**
+ * This is the activity class for the AndroidShip game.
+ *
+ *  @author Ward Bonnefond (wardwdb)
+ *  @author Tyler Ritchie (ritchie4)
+ *  @author Nick Crowder (crowdern)
+ *  @version Apr 16, 2012
+ */
+public class AndroidShipActivity
+    extends Activity
+{
+
+    private AndroidShipBoard playerOneBoard;
+    private AndroidShipBoard playerTwoBoard;
+    private AndroidShipView  shipView;
+
+    private TextView   playerLabel;
+    private TextView   statusLabel;
+    private CheckBox   checkBox;
+
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState
+     *            previous state saved by the last run of the activity
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
+
+        shipView = (AndroidShipView)findViewById(R.id.mazeView);
+        /*
+        checkBox = (CheckBox)findViewById(R.id.drawEraseMode);
+        statusLabel = (TextView)findViewById(R.id.statusLabel);
+
+        make the view components
+        */
+
+        playerOneBoard = new AndroidShipBoard();
+        playerTwoBoard = new AndroidShipBoard();
+
+        // mazeView.setModel(maze);
     }
+
+    /**
+     * This handles when the maze solver button is clicked
+     *
+     * @param view
+     *            The view to be dealt with
+     */
+    public void fireButtonClicked(View view)
+    {
+        //
+    }
+
+    /**
+     * This returns the MazeView object being used.
+     *
+     * @return The MazeView object
+     */
+    public AndroidShipView getView()
+    {
+        return shipView;
+    }
+
+    // maybe a getBoard method
 }
--- AndroidShip/src/cs2114/AndroidShip/Cell.java
+++ AndroidShip/src/cs2114/AndroidShip/Cell.java
@@ -1,6 +1,89 @@
-package cs2114.AndroidShip;
+package cs2114.mazesolver;
 
+import android.graphics.Paint;
+
+/**
+ *  This defines a cell on the AndroidShip board.
+ *
+ *  @author Ward Bonnefond (wardwdb)
+ *  @author Tyler Ritchie (ritchie4)
+ *  @author Nick Crowder (crowdern)
+ *  @version Apr 16, 2012
+ */
 public class Cell
 {
-    //TEST
+    /**
+     * The enumerated type for a water cell.
+     */
+    public final static int WATER = 0;
+
+    /**
+     * The enumerated type for a ship cell.
+     */
+    public final static int SHIP = 1;
+
+    /**
+     * The enumerated type for a hit cell.
+     */
+    public final static int HIT = 2;
+
+    /**
+     * The enumerated type for a misfire cell.
+     */
+    public final static int MISS = 3;
+
+    private Paint brush;
+    private int currentType;
+
+    /**
+     * Creates a new AndroidShip Cell object.
+     * @param type The type of the cell
+     */
+    public Cell(int type)
+    {
+        currentType = type;
+    }
+
+    /**
+     * This sets the cell's type to a number for the enumerated types.
+     * @param type The cell's enumerated type number
+     */
+    public void setType(int type)
+    {
+        currentType = type;
+        setPaint();
+    }
+
+    /**
+     * This returns the type of the cell.
+     * @return The type of the cell
+     */
+    public int getType()
+    {
+        return currentType;
+    }
+
+    public void setPaint()
+    {
+        switch(currentType)
+        {
+            case WATER: brush.setColor(color);
+                break;
+
+            case SHIP:
+                break;
+
+            case HIT:
+                break;
+
+            case MISS:
+                break;
+
+        }
+    }
+
+    public Paint getPaint()
+    {
+        return
+    }
 }
