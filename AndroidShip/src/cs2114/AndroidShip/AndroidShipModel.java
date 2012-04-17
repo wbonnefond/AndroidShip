cce7cb42998c15a4b7eecb8550cb8e2d5ed4eaff
ritchie4 <ritchie4@vt.edu>
2012-04-17 00:55:11 -0400

    Added a comment

--- AndroidShip/src/cs2114/AndroidShip/AndroidShipModel.java
+++ AndroidShip/src/cs2114/AndroidShip/AndroidShipModel.java
@@ -1,5 +1,14 @@
 package cs2114.AndroidShip;
 
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author Tyler
+ *  @version Apr 17, 2012
+ */
 public class AndroidShipModel
 {
 
