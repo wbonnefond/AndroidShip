b0d5a04609580484a4aa5d56721399b8c552fa74
ritchie4 <ritchie4@vt.edu>
2012-04-17 21:57:57 -0400

    The test case for the model was added.

---
+++ AndroidShip/src/cs2114/AndroidShip/AndroidShipModelTest.java
@@ -0,0 +1,23 @@
+package cs2114.AndroidShip;
+
+import student.TestCase;
+
+/**
+ *  This tests the AndroidShip model class.
+ *
+ *  @author Ward Bonnefond (wardwdb)
+ *  @author Tyler Ritchie (ritchie4)
+ *  @author Nick Crowder (crowdern)
+ *  @version Apr 16, 2012
+ */
+public class AndroidShipModelTest extends TestCase
+{
+    /**
+     * Initializes all fields necessary for the tests.
+     */
+    public void setUp()
+    {
+
+    }
+
+}
