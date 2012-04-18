487a786d2113d88400a4fb7f551f67862dd69b46
ritchie4 <ritchie4@vt.edu>
2012-04-17 21:43:34 -0400

    Fixed enumerated types in Cell. Got rid of CellTest.

--- AndroidShip/src/cs2114/AndroidShip/Cell.java
+++ AndroidShip/src/cs2114/AndroidShip/Cell.java
@@ -12,59 +12,7 @@ import android.graphics.Paint;
  */
 public class Cell
 {
-    /**
-     * The enumerated type for a misfired cell.
-     */
-    public final static int INVALID_CELL = 0;
-
-    /**
-     * The enumerated type for a water cell.
-     */
-    public final static int WATER = 1;
-
-    /**
-     * The enumerated type for a ship cell.
-     */
-    public final static int SHIP = 2;
-
-    /**
-     * The enumerated type for a hit cell.
-     */
-    public final static int HIT = 3;
-
-    /**
-     * The enumerated type for a misfired cell.
-     */
-    public final static int MISS = 4;
-
-    private Paint brush;
-    private int currentType;
-
-    /**
-     * Creates a new AndroidShip Cell object.
-     * @param type The type of the cell
-     */
-    public Cell(int type)
-    {
-        currentType = type;
+    public enum Cells {
+        INVALID_CELL, WATER, SHIP, HIT, MISS
     }
-
-    /**
-     * This sets the cell's type to a number for the enumerated types.
-     * @param type The cell's enumerated type number
-     */
-    public void setType(int type)
-    {
-        currentType = type;
-    }
-
-    /**
-     * This returns the type of the cell.
-     * @return The type of the cell
-     */
-    public int getType()
-    {
-        return currentType;
-    }
-
 }
