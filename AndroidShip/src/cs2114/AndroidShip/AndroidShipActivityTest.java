c028c9ea09539c5e71258172b4d906959b46fbc3
ritchie4 <ritchie4@vt.edu>
2012-04-17 21:55:26 -0400

    The test cases for the activity, view, and the board class.

---
+++ AndroidShip/src/cs2114/AndroidShip/AndroidShipActivityTest.java
@@ -0,0 +1,23 @@
+package cs2114.AndroidShip;
+
+import student.TestCase;
+
+/**
+ *  This tests the AndroidShip activity class.
+ *
+ *  @author Ward Bonnefond (wardwdb)
+ *  @author Tyler Ritchie (ritchie4)
+ *  @author Nick Crowder (crowdern)
+ *  @version Apr 16, 2012
+ */
+public class AndroidShipActivityTest extends TestCase
+{
+    /**
+     * Initializes all fields necessary for the tests.
+     */
+    public void setUp()
+    {
+
+    }
+
+}
